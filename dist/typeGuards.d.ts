adaa0076c1dfec9919e128cafd7574a223a14afc
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2022-07-22 22:31:00 +0300

    Gitignore and delete dist

--- dist/typeGuards.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const isNumList: (values: any) => values is number[];
--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { ParamLists, TrainAndTest, TrainAndTestLabels } from "./types";
-export declare const shuffleList: (inputs: number[][], labels: number[]) => ParamLists;
-export declare const isSingleVariable: (inputs: any, labels: any) => boolean;
-export declare const checkMultiValues: (multiInputs: any, labels: any) => boolean;
-export declare const isNumList: (values: any) => values is number[];
-export declare const splitToChunks: (blockSize: number, list: number[][]) => TrainAndTest;
-export declare const splitToChunksLabels: (blockSize: number, list: number[]) => TrainAndTestLabels;
